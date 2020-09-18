244608ae21067c2ff5ec35b2b0460275cd769fc2
ilein <ilein2005@inbox.ru>
2020-09-19 03:58:19 +0500

    add liquibase

--- src/main/java/com/gdtest/investment/InvestmentApplication.java
+++ src/main/java/com/gdtest/investment/InvestmentApplication.java
@@ -4,13 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.activation.DataSource;
-
 @SpringBootApplication
 public class InvestmentApplication {
     @Autowired
     public static void main(String[] args) {
         SpringApplication.run(InvestmentApplication.class, args);
     }
-
 }
